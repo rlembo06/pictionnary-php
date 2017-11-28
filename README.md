f7f773f511bb72fc2f65d24969c2f4e534951986
LEMBO Romain <lr706510@iutnice.unice.fr>
2017-11-28 16:46:23 +0100

    Answers for questions ok, simple form ok

---
+++ README.md
@@ -0,0 +1,33 @@
+1)
+Q :
+c'est quoi les attributs action et method ?
+
+R :
+Attribut "action" : Permet de spécifier une URI pour éffectuer le traitement du formulaire.
+Attribut "method" : Permet de spécifier la méthode HTTP pour envoyer les données vers le serveur (GET, POST, PUT, DELETE, etc).
+//-------------------------------//
+
+2)
+Q :
+qu'y a-t-il d'autre comme possiblité que post pour l'attribut method ?
+
+R :
+Il y a GET, PUT, DELETE, OPTIONS, TRACE, HEAD, CONNECT, PATCH).
+//-------------------------------//
+
+3)
+Q :
+quelle est la différence entre les attributs name et id ?
+
+R :
+Attribut "name" : Pour identifier le champs d'un formulaire lors de l'envoie de ce dernier vers le serveur.
+Attribut "id" : Est un identifiant unique qui doit être unique pour l'ensemble du document. Pour modifier son style CSS par exemple ou lui attribuer une fonction en JavaScript.
+//-------------------------------//
+
+4)
+Q :
+c'est lequel qui doit être égal à l'attribut for du label ?
+
+R :
+C'est l'attribut name qui doit avoir le même nom que l'attribut for du label.
+//-------------------------------//
--- inscription.php
+++ inscription.php
@@ -1,37 +1,78 @@
 <!DOCTYPE html>  
-<html>  
-<head>  
-    <meta charset=utf-8 />  
-    <title>Pictionnary - Inscription</title>  
-</head>  
-<body>  
-  
-<h2>Inscrivez-vous</h2>  
-<form class="inscription" action="req_inscription.php" method="post" name="inscription">  
-    <!-- c'est quoi les attributs action et method ? -->  
-    <!-- qu'y a-t-il d'autre comme possiblité que post pour l'attribut method ? -->  
-    <span class="required_notification">Les champs obligatoires sont indiqués par *</span>  
-    <ul>  
-        </li>  
-        <li>  
-            <label for="email">E-mail :</label>  
-            <input type="email" name="email" id="email"/>  
-            <!-- ajouter à input l'attribut qui lui donne le focus automatiquement -->  
-            <!-- ajouter à input l'attribut qui dit que c'est un champs obligatoire -->  
-            <!-- quelle est la différence entre les attributs name et id ? -->  
-            <!-- c'est lequel qui doit être égal à l'attribut for du label ? -->   
-            <span class="form_hint">Format attendu "name@something.com"</span>  
-        </li>  
-        <li>  
-            <label for="prenom">Prénom :</label>  
-            <input type="text" name="prenom" id="prenom"/>  
-            <!-- ajouter à input l'attribut qui dit que c'est un champs obligatoire -->  
-            <!-- ajouter à input l'attribut qui donne une indication grisée (placeholder) -->  
-        </li>  
-        <li>  
-            <input type="submit" value="Soumettre Formulaire">  
-        </li>  
-    </ul>  
-</form>  
-</body>  
+<html>
+    
+    <head>  
+        <meta charset=utf-8 />  
+        <title>Pictionnary - Inscription</title>
+        <meta name="description" content="TP - Lembo Romain">  
+    </head> 
+    
+    <body>  
+        
+        <header>
+            <h1>Inscription</h1>
+        </header>
+        
+        <main>
+            
+            <section>
+                
+                <h2>Inscrivez-vous</h2> 
+                <form class="inscription" action="req_inscription.php" method="post" name="inscription">  
+
+                    <span class="required_notification">Les champs obligatoires sont indiqués par *</span>  
+                    
+                    <ul>  
+                        <li>  
+                            <label for="email">E-mail* :</label>  
+                            <input type="email" name="email" id="email" autofocus required/>  
+                            <span class="form_hint">Format attendu "name@something.com"</span>  
+                        </li>  
+                        <li>  
+                            <label for="prenom">Prénom* :</label>  
+                            <input type="text" name="prenom" id="prenom" required placeholder="Prénom"/>  
+                        </li>
+                        <li>  
+                            <label for="nom">Nom :</label>  
+                            <input type="text" name="nom" id="nom" />  
+                        </li>
+                        <li>  
+                            <label for="telephone">Téléphone :</label>  
+                            <input type="tel" name="telephone" id="telephone" />  
+                        </li>
+                        <li>  
+                            <label for="siteweb">Site web :</label>  
+                            <input type="url" name="siteweb" id="siteweb" />  
+                        </li>
+                        <li>  
+                            <label for="homme">Homme :</label>  
+                            <input type="radio" name="sexe" id="homme" value="homme"/>
+                            <label for="femme">Femme :</label>  
+                            <input type="radio" name="sexe" id="femme" value="femme"/> 
+                        </li>
+                        <li>  
+                            <label for="ville">Ville :</label>  
+                            <input type="text" name="ville" id="ville" />  
+                        </li>
+                        <li>  
+                            <label for="taille">Taille :</label>  
+                            <input type="range" min="0" max="2.50" step="0.01" name="taille" id="taille" />
+                        </li>
+                        <li>  
+                            <label for="couleur">Couleur préférée :</label>  
+                            <input type="color" name="couleur" id="couleur" value="#000000"/>
+                        </li>
+                        <li>  
+                            <input type="submit" value="Soumettre Formulaire"> 
+                        </li>  
+                    </ul> 
+                    
+                </form>
+                
+            </section>
+            
+        </main>
+          
+    </body> 
+    
 </html> 
